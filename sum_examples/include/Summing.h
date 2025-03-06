28ce404723c448606359bbc771ab0ec9a79c78d8
Michael McLeod <michaelmcleod@ucl.ac.uk>
2025-03-06 22:57:28 +0000

    Add summation examples

---
+++ sum_examples/include/Summing.h
@@ -0,0 +1,103 @@
+#ifndef SUMMING_H
+#define SUMMING_H
+
+// x86 without AVX
+//    #include <xmmintrin.h>
+
+// x86 with AVX
+    #include <immintrin.h>
+
+// ARM
+//    #include <arm_neon.h>
+
+
+#include <vector>
+#include <iostream>
+using std::vector;
+
+template<typename T>
+T trivial_sum(const vector<T> &vals)
+{
+    T sum = 0;
+    for(auto &x : vals)
+    {
+        sum = sum + x;
+    }
+    return sum;
+}
+
+template<typename FPtype>
+FPtype Kahan_sum(const vector<FPtype> &vals)
+{
+    FPtype sum = 0;
+    FPtype c = 0;
+    FPtype y, t;
+    for (auto &x : vals)
+    {
+        y = x - c;
+        t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
+
+    return sum;
+}
+
+//x86 with AVX
+    const size_t W = 8;
+    using VEC = __m256;
+    inline VEC SET_ZERO() { return _mm256_setzero_ps(); }
+    inline VEC ADD(VEC a, VEC b) { return _mm256_add_ps(a, b); }
+    inline VEC SUB(VEC a, VEC b) { return _mm256_sub_ps(a, b); }
+    inline VEC LOAD(const float *p) { return _mm256_load_ps(p); }
+    inline void STORE(float* p, VEC a) { return _mm256_store_ps(p, a); }
+
+//x86 without AVX
+//    const size_t W = 4;
+//    using VEC = __m128;
+//    inline VEC SET_ZERO() { return _mm_setzero_ps(); }
+//    inline VEC ADD(VEC a, VEC b) { return _mm_add_ps(a, b); }
+//    inline VEC SUB(VEC a, VEC b) { return _mm_sub_ps(a, b); }
+//    inline VEC LOAD(const float *p) { return _mm_load_ps(p); }
+//    inline void STORE(float* p, VEC a) { return _mm_store_ps(p, a); }
+
+//ARM
+//    const size_t W = 4;
+//    using VEC = float32x4_t;
+//    inline VEC SET_ZERO() { return vdupq_n_f32(0); }
+//    inline VEC ADD(VEC a, VEC b) { return vaddq_f32(a, b); }
+//    inline VEC SUB(VEC a, VEC b) { return vsubq_f32(a, b); }
+//    inline VEC LOAD(const float *p) { return vld1q_f32(p); }
+//    inline void STORE(float* p, VEC a) { return vst1q_f32(p, a); }
+
+
+// SIMD Kahan
+// size of vals should be multiple of W; can be padded with 0s at the end
+// better if vals is appropriately aligned
+float SIMD_sum(const float *vals, const size_t N)
+{
+    size_t N_vec = N / W;
+    
+    float *sums = new (std::align_val_t(32)) float[W];
+
+    VEC c_vals = SET_ZERO();
+    VEC t_vals = SET_ZERO();
+    for (size_t i = 0; i < N_vec; i++)
+    {
+        VEC xs = LOAD(vals + (i*W));  // pointer arithmetic to get the data address
+        VEC y_vals = SUB(xs, c_vals);  // y = x - c. Note we don't need to store y_vals in RAM.
+        VEC s_vals = t_vals;
+        t_vals = ADD(s_vals, y_vals);  // t = sum + y. 
+        c_vals = SUB(SUB(t_vals, s_vals), y_vals);  // c = (t-sum) - y
+    }
+    STORE(sums, t_vals);
+    float tot_sum = 0;
+    for(int i = 0; i < W; i++)
+    {
+        tot_sum += sums[i];  // probably don't need compensated mini-sum here because partial sums should be similar size
+    }
+
+    return tot_sum;
+}
+
+#endif
---
+++ sum_examples/source/Summation.cpp
@@ -0,0 +1,51 @@
+#include "Summing.h"
+#include <iostream>
+#include <vector> 
+#include <chrono>
+#include <numeric>
+
+using std::vector;
+
+typedef std::chrono::high_resolution_clock myClock;
+
+double t_seconds(std::chrono::time_point<myClock> t1, std::chrono::time_point<myClock> t2)
+{
+    return (t2 - t1).count() / 1e9;
+}
+
+int main()
+{
+    long N = 160'000'000l;
+    float val = 1e-6;
+    float *xs = new (std::align_val_t(32)) float[N];
+    vector<float> v(N);
+    
+    // initialise data
+    for(size_t i = 0; i < N; i++)
+    {
+        v[i] = val;
+        xs[i] = val;
+    }
+
+    std::cout << "Addresses = " << v.data() << " " << xs << std::endl;
+
+    float triv = trivial_sum(v);
+
+    auto tk1 = myClock::now();
+    float k = Kahan_sum(v);
+    auto tk2 = myClock::now();
+
+    auto ts1 = myClock::now();
+    float S = SIMD_sum(xs, N);
+    auto ts2 = myClock::now();
+
+    auto tl1 = myClock::now();
+    float STD = std::accumulate(v.begin(), v.end(), 0.0f);
+    auto tl2 = myClock::now();
+
+    printf("trivial sum = %f, Kahan sum = %f, SIMD sum = %f; expected 160.0\n", triv, k, S);
+
+    printf("Timing for regular Kahan = %f, and SIMD Kahan = %f, and ratio = %f\n", t_seconds(tk1, tk2), t_seconds(ts1, ts2), t_seconds(tk1, tk2)/t_seconds(ts1, ts2));
+    printf("std::accumulate = %f, Timing for std = %f\n", STD, t_seconds(tl1, tl2));
+    return 0;
+}
